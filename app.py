2457b6d90bdd490789ba60e02f5e9ed049ac56b5
ryaniqs <130678402+ryaniqs@users.noreply.github.com>
2023-09-06 09:17:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,16 @@
 # app.py
 import streamlit as st
-import folium
-from streamlit_folium import folium_static
-import json
 
 def main():
-    st.title("Draw a Square on the Map")
-    
-    m = folium.Map(location=[20, 0], zoom_start=2)
-    draw = folium.plugins.Draw(draw_options={
-        'polygon': {'allowIntersection': False},
-        'circle': False, 
-        'polyline': False, 
-        'circleMarker': False,
-        'marker': False
-    })
-    draw.add_to(m)
-    
-    folium_static(m)
-    
-    geo_json_data = st.session_state.get("geo_json", None)
+    st.title("Streamlit app with geojson.io")
 
-    if st.button("Confirm Square"):
-        textarea_val = st.text_area("GeoJSON Data", value="", key="geo_data")
-        if textarea_val:
-            geo_json_data = json.loads(textarea_val)
-            st.session_state.geo_json = geo_json_data
-            
-            if st.button("Is this your area of interest?"):
-                st.download_button("Download GeoJSON", data=json.dumps(geo_json_data), file_name="geojson_data.geojson", mime="application/geo+json")
+    # Create an iframe that embeds geojson.io
+    iframe_code = '<iframe src="http://geojson.io" width="100%" height="600px"></iframe>'
+    st.markdown(iframe_code, unsafe_allow_html=True)
 
-    if st.button("Erase and Start Again"):
-        st.experimental_rerun()  # This will rerun the streamlit app
+    st.write("""
+    Above is an embedded version of geojson.io. You can draw, edit, and export GeoJSON directly from this interface.
+    """)
 
 if __name__ == "__main__":
     main()
