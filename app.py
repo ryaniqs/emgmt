880d0bf41e911e08ea3dbf91dbeba9391647392c
ryaniqs <130678402+ryaniqs@users.noreply.github.com>
2023-09-06 11:05:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -80,9 +80,4 @@ if __name__ == "__main__":
     download_geojson(output)
 
 # Load a smaller image with a clickable link
-st.sidebar.image("https://images.squarespace-cdn.com/content/v1/5e8cc689c426331984ffa7f1/1586545924842-ZUPOCD4I7SX1OGIBHDS3/Logo_IQSpatialLogo.png?format=1500w", width=100)
-st.sidebar.markdown("[![IQSpatial Logo](https://images.squarespace-cdn.com/content/v1/5e8cc689c426331984ffa7f1/1586545924842-ZUPOCD4I7SX1OGIBHDS3/Logo_IQSpatialLogo.png?format=1500w)](https://iqspatial.com/)")
-
-
-
-
+st.sidebar.image("https://images.squarespace-cdn.com/content/v1/5e8cc689c426331984ffa7f1/1586545924842-ZUPOCD4I7SX1OGIBHDS3/Logo_IQSpatialLogo.png?format=1500w", width=100, use_container_width=False, link="https://iqspatial.com/")
